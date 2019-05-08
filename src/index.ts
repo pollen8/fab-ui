2863f41ba347bd6a202025ba5a253e3fc6c6502f
Rob Clayburn <rob@cognitivelogic.com>
2019-05-08 23:59:20 +0100

    add Label

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { Grid } from './Grid';
 export { default as Modal } from 'react-responsive-modal';
 export {
   Row, Col, Container, Content, SubHeading, Card, Name, CardBody, SlidePanelBody, Description,
-  Input, Button, FormGroup, TextArea, ModalBody, ModalFooter, Alert
+  Input, Button, FormGroup, TextArea, ModalBody, ModalFooter, Alert, Label
 } from './Layout';
